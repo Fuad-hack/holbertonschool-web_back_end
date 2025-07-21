8e8165a589f355cb8466af1ec0f18c879bc1615b
root <you@example.com>
2025-07-21 08:33:21 +0000

    Task - 8

---
+++ NoSQL/8-all.py
@@ -0,0 +1,11 @@
+# Task - 8
+
+from pymongo import MongoClient
+list_all = __import__('8-all').list_all
+
+client = MongoClient('mongodb://127.0.0.1:27017')
+school_collection = client.my_db.school
+schools = list_all(school_collection)
+for school in schools:
+    print("[{}] {}".format(school.get('_id'), school.get('name')))
+
